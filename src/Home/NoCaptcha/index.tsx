8901b3ad5c9d95e6d011d1b078b7a055d62a9cf3
Igor Osipov <igor.osipov@ottofeller.com>
2023-01-11 17:20:05 +0100

    implement Home page

---
+++ src/Home/NoCaptcha/index.tsx
@@ -0,0 +1,46 @@
+import cn from 'classnames'
+import {Link} from 'common/Button'
+import {memo} from 'react'
+
+export const NoCaptcha = memo(function NoCaptcha() {
+  return (
+    <div className="grid grid-cols-container px-4 md:px-8 mt-24 md:mt-48 pb-32 md:pb-0">
+      <div
+        className={cn(
+          'relative rounded-64 bg-gradient-81.5 from-4940e0 to-a39dff col-start-2 py-14 md:py-28 px-10 md:px-20',
+        )}
+      >
+        <div className="justify-items-start grid gap-y-12 max-w-[620px]">
+          <div className="grid gap-y-4">
+            <h2 className="font-semibold text-32 md:text-48">Say goodbye to CAPTCHA and bots</h2>
+
+            <p className="text-ffffff/70 text-18">
+              Secure your Discord with <span className="text-ffffff font-semibold">World ID Bot</span>
+            </p>
+          </div>
+
+          {/* FIXME: add link */}
+          <Link
+            variant="flat"
+            className="justify-self-stretch md:justify-self-start bg-ffffff"
+            href={process.env.REACT_APP_ADD_BOT_URL || '#'}
+          >
+            <span className="bg-gradient-81.5 from-4940e0 to-a39dff bg-clip-text text-transparent">Install now</span>
+          </Link>
+        </div>
+
+        <div className="mt-10 pb-20 md:mt-0 md:pb-0">
+          <img
+            className={cn(
+              'absolute [filter:drop-shadow(0px_4px_12px_rgba(0,0,0,.08))_drop-shadow(0px_8px_64px_rgba(0,0,0,0.16))]',
+              'w-[calc(100%-40px)] left-5',
+              'md:left-[unset] md:w-auto md:top-10 md:right-14 md:h-[calc(100%+40px)]',
+            )}
+            src="/images/discord.png"
+            alt="Discord"
+          />
+        </div>
+      </div>
+    </div>
+  )
+})
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,9 +1,50 @@
+import {Footer} from 'common/Footer'
+import {Header} from 'common/Header'
+import {Icon} from 'common/Icon'
+import {Layout} from 'common/Layout'
 import {memo} from 'react'
+import {About} from './About'
+import {Benefits} from './Benefits'
+import {CTA} from './CTA'
+import {Intro} from './Intro'
+import {Join} from './Join'
+import {NoCaptcha} from './NoCaptcha'
+import {Steps} from './Steps'
 
 export const Home = memo(function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <p className="text-red-500 text-5xl">Discord Bouncer</p>
-    </div>
+    <Layout>
+      <div className="grid grid-rows-auto/fr/auto min-h-screen">
+        <Header />
+        <Intro />
+
+        <CTA wrapperClassName="mt-16 md:mt-0">
+          <span>
+            Welcome to the World ID Discord Bot, if you want to use this in your server to verify proof-of-personhood,
+            follow these instructions to get started.
+          </span>
+
+          <a href="https://github.com/worldcoin/world-id-discord-bot" target="_blank" rel="noreferrer">
+            <Icon className="w-6 h-6" name="github" />
+          </a>
+        </CTA>
+      </div>
+
+      <About />
+      <Steps />
+      <NoCaptcha />
+      <Benefits />
+      <Join />
+
+      <CTA wrapperClassName="mt-32 md:mt-64">
+        <span>This project is open sourced and anyone can contribute to it.</span>
+
+        {/* TODO: validate url */}
+        <a href="https://github.com/worldcoin/world-id-discord-bot" target="_blank" rel="noreferrer">
+          <Icon className="w-6 h-6" name="github" />
+        </a>
+      </CTA>
+      <Footer />
+    </Layout>
   )
 })
