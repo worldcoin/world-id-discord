cf68d3b2f045d8a0edbc715c919c6ba69879f19d
Mickh F <michael.furutin@ottofeller.com>
2023-01-15 16:58:09 +0300

    fix: add identify scope

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,7 +5,7 @@ import {createGuildCommands} from 'services/discord'
 
 const clientId = process.env.DISCORD_APP_ID!
 const clientSecret = process.env.DISCORD_APP_SECRET!
-const scopes = ['bot', 'applications.commands'].join(' ')
+const scopes = ['identify', 'bot', 'applications.commands'].join(' ')
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   return await NextAuth(req, res,{
