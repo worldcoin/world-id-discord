ac1c2a98f0b1aea0f99273c053fae0aa0f854890
Igor Osipov <igor.osipov@ottofeller.com>
2023-01-17 12:27:14 +0100

    adjust admin page UI

--- src/Admin/index.tsx
+++ src/Admin/index.tsx
@@ -1,6 +1,8 @@
 import cn from 'classnames'
 import {Button} from 'common/Button'
-import {ModalPageLayout} from 'common/ModalPageLayout'
+import {Header} from 'common/Header'
+import {Layout} from 'common/Layout'
+import {Modal} from 'common/Modal'
 import Image from 'next/image'
 import {memo, useCallback, useState} from 'react'
 import {RolesSelector} from './RolesSelector'
@@ -32,84 +34,90 @@ export const Admin = memo(function Admin() {
   }
 
   return (
-    <ModalPageLayout loading={formDataLoading}>
-      <div className="relative grid justify-center auto-cols-max items-center p-6 border-b border-[color:inherit]">
-        <div className="grid gap-y-3 justify-items-center w-full">
-          <span className="text-20 font-semibold">Discord Bouncer Admin</span>
+    <Layout className="bg-0d1020 flex justify-center items-center relative min-h-screen">
+      <Image src="/images/admin/background.svg" alt="Background" fill className="object-cover" />
 
-          <div className="grid items-center grid-cols-fr/auto py-2 px-3 bg-ffffff/10 rounded-lg gap-x-2">
-            {/* FIXME: pass real data  */}
-            <Image src={guildData.image} alt="Discord guild logo" width={24} height={24} className="rounded-full" />
-            <span>{guildData.name}</span>
+      <Header hideLinks onTop />
+
+      <Modal loading={formDataLoading}>
+        <div className="relative grid justify-center auto-cols-max items-center p-6 border-b border-[color:inherit]">
+          <div className="grid gap-y-3 justify-items-center w-full">
+            <span className="text-20 font-semibold">Discord Bouncer Admin</span>
+
+            <div className="grid items-center grid-cols-fr/auto py-2 px-3 bg-ffffff/10 rounded-lg gap-x-2">
+              {/* FIXME: pass real data  */}
+              <Image src={guildData.image} alt="Discord guild logo" width={24} height={24} className="rounded-full" />
+              <span>{guildData.name}</span>
+            </div>
+          </div>
+
+          <StyledCheckbox className="absolute top-6 right-6" />
+        </div>
+
+        <div className="grid grid-cols-[100%] gap-y-8.5 px-8 pt-12 pb-4">
+          <div className="grid gap-y-2">
+            <span>Credentials</span>
+
+            <p className="font-rubik text-14 text-ffffff/40">
+              The server configuration allows you to manage the various components of your Discord Bouncer.
+            </p>
+          </div>
+
+          <div className="grid gap-y-8">
+            <RolesSelector
+              icon="mobile-device"
+              name="Phone Number"
+              description="A single-use code will be delivered via SMS."
+              className="mt-4"
+              roles={roles}
+              setRoles={setRoles}
+              selectedRoles={selectedPhoneRoles}
+              setSelectedRoles={setSelectedPhoneRoles}
+            />
+
+            <RolesSelector
+              icon="orb"
+              name="Orb"
+              description="Completely private iris imaging with a device called an orb."
+              className="mt-4"
+              roles={roles}
+              setRoles={setRoles}
+              selectedRoles={selectedOrbRoles}
+              setSelectedRoles={setSelectedOrbRoles}
+            />
+          </div>
+
+          <div className="grid justify-items-center gap-y-4 mt-12">
+            <Button
+              className="w-full font-sora disabled:opacity-20"
+              disabled={!formValid || savingInProgress}
+              onClick={saveChanges}
+            >
+              {savingInProgress ? 'Saving...' : 'Save changes'}
+            </Button>
+
+            <span
+              className={cn(
+                'transition-visibility/opacity col-start-1 row-start-2',
+                {'visible opacity-100': savedSuccessfully === true},
+                {'invisible opacity-0': savedSuccessfully !== true},
+              )}
+            >
+              Your changes have been successfully saved!
+            </span>
+
+            <span
+              className={cn(
+                'transition-visibility/opacity col-start-1 row-start-2 text-red-500',
+                {'visible opacity-100': savedSuccessfully === false},
+                {'invisible opacity-0': savedSuccessfully !== false},
+              )}
+            >
+              Something went wrong. Try again, please.
+            </span>
           </div>
         </div>
-
-        <StyledCheckbox className="absolute top-6 right-6" />
-      </div>
-
-      <div className="grid grid-cols-[100%] gap-y-8.5 px-8 pt-12 pb-4">
-        <div className="grid gap-y-2">
-          <span>Credentials</span>
-
-          <p className="font-rubik text-14 text-ffffff/40">
-            The server configuration allows you to manage the various components of your Discord Bouncer.
-          </p>
-        </div>
-
-        <div className="grid gap-y-8">
-          <RolesSelector
-            icon="mobile-device"
-            name="Phone Number"
-            description="A single-use code will be delivered via SMS."
-            className="mt-4"
-            roles={roles}
-            setRoles={setRoles}
-            selectedRoles={selectedPhoneRoles}
-            setSelectedRoles={setSelectedPhoneRoles}
-          />
-
-          <RolesSelector
-            icon="orb"
-            name="Orb"
-            description="Completely private iris imaging with a device called an orb."
-            className="mt-4"
-            roles={roles}
-            setRoles={setRoles}
-            selectedRoles={selectedOrbRoles}
-            setSelectedRoles={setSelectedOrbRoles}
-          />
-        </div>
-
-        <div className="grid justify-items-center gap-y-4 mt-12">
-          <Button
-            className="w-full font-sora disabled:opacity-20"
-            disabled={!formValid || savingInProgress}
-            onClick={saveChanges}
-          >
-            {savingInProgress ? 'Saving...' : 'Save changes'}
-          </Button>
-
-          <span
-            className={cn(
-              'transition-visibility/opacity col-start-1 row-start-2',
-              {'visible opacity-100': savedSuccessfully === true},
-              {'invisible opacity-0': savedSuccessfully !== true},
-            )}
-          >
-            Your changes have been successfully saved!
-          </span>
-
-          <span
-            className={cn(
-              'transition-visibility/opacity col-start-1 row-start-2 text-red-500',
-              {'visible opacity-100': savedSuccessfully === false},
-              {'invisible opacity-0': savedSuccessfully !== false},
-            )}
-          >
-            Something went wrong. Try again, please.
-          </span>
-        </div>
-      </div>
-    </ModalPageLayout>
+      </Modal>
+    </Layout>
   )
 })
