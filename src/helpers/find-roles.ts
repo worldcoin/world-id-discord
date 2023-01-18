34718f77938227be076a22e18e2237ef8b5f1864
Mickh F <michael.furutin@ottofeller.com>
2023-01-18 18:56:59 +0300

    feat: show only permitted roles

--- src/helpers/find-roles.ts
+++ src/helpers/find-roles.ts
@@ -1,12 +1,15 @@
 import {APIRole} from 'discord-api-types/v10'
 
 export const findRoles = (configRoles: Array<string>, allRoles: Array<APIRole>) => {
-  return configRoles.map((roleId) => {
-    const role = allRoles.find((r) => r.id === roleId) as APIRole
-
-    return {
-      label: role.name,
-      value: role.id,
+  const roles = []
+  for (const configRole of configRoles) {
+    const role = allRoles.find((r) => r.id === configRole)
+    if (role) {
+      roles.push({
+        label: role.name,
+        value: role.id,
+      })
     }
-  })
+  }
+  return roles
 }
