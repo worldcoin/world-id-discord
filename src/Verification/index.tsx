2adbc104eb7b60beb5a75618fd481f07e1e48ac7
Mickh F <michael.furutin@ottofeller.com>
2023-01-27 16:17:12 +0300

    fix: improve markup

--- src/Verification/index.tsx
+++ src/Verification/index.tsx
@@ -89,7 +89,7 @@ export const Verification = memo(function Verification(props: {
       </Modal>
 
       <InfoLine
-        className="fixed bottom-0 inset-x-0"
+        className="fixed bottom-0 inset-x-0 z-10"
         text="The Discord Bouncer helps prevent spam and increase the quality of the community by making sure everyone who joins is a human. "
       />
     </Layout>
