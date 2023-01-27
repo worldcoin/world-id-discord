1e1df0d2a9a47040d6072299a7074b337e5494c1
michaelfurutin <104859710+michaelfurutin@users.noreply.github.com>
2023-01-27 18:22:56 +0300

    fix: improve markup (#58)
    * fix: improve markup
    
    * add padding & hide logo on scroll bottom
    
    ---------
    
    Co-authored-by: Igor Osipov <igor.osipov@ottofeller.com>

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -1,15 +1,36 @@
 import cn from 'classnames'
 import { Icon } from 'common/Icon'
 import Link from 'next/link'
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
+import { useScrollDirection } from 'react-use-scroll-direction'
 
 export const Header = memo(function Header(props: { hideLinks?: boolean; onTop?: boolean }) {
+  const { isScrollingUp, isScrollingDown } = useScrollDirection()
+  const [isVisible, setIsVisible] = useState<boolean>(true)
+
+  // NOTE: hide header when scrolling down
+  useEffect(() => {
+    if (!props.onTop) {
+      return
+    }
+
+    if (isScrollingDown) {
+      setIsVisible(false)
+    }
+
+    if (isScrollingUp) {
+      setIsVisible(true)
+    }
+  }, [isScrollingDown, isScrollingUp, props.onTop])
+
   return (
     <div
       className={cn(
-        'z-10 grid py-8 grid-cols-container-wide px-4 md:px-8',
+        'z-10 grid py-8 grid-cols-container-wide px-4 md:px-8 transition-opacity',
         { relative: !props.onTop },
         { 'fixed top-0 inset-x-0': props.onTop },
+        { 'opacity-0': props.onTop && !isVisible },
+        { 'opacity-100': props.onTop && isVisible },
       )}
     >
       <div className="grid items-center justify-between grid-flow-col col-start-2">
