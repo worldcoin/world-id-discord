157d1bd4da96c819b666838c2e63ae525e9bee46
Mickh F <michael.furutin@ottofeller.com>
2023-02-09 16:54:38 +0300

    feat: update benefits section

--- src/Home/Benefits/index.tsx
+++ src/Home/Benefits/index.tsx
@@ -5,20 +5,22 @@ import { memo } from 'react'
 const items = [
   {
     title: 'Only humans',
-    content: 'Protect certain actions so only humans can perform them.',
+    content: ['Protect certain actions so only', 'humans can perform them.'],
   },
   {
     title: 'Complete flexibility',
-    content:
-      'The Discord Bouncer will assign a role to verified humans. You can choose what users can do with that role.',
+    content: [
+      'The Discord Bouncer will assign a role to verified humans.',
+      'You can choose what users can do with that role.',
+    ],
   },
   {
     title: 'Anonymous and private',
-    content: 'No personal information needed or shared. Just verify humans.',
+    content: ['No personal information needed or shared.', 'Just verify humans.'],
   },
   {
     title: 'Better community controls',
-    content: 'Now when you ban a user you can be sure they won’t return with another account.',
+    content: ['Now when you ban a user you can be sure', 'they won’t return with another account.'],
   },
 ]
 
@@ -26,29 +28,37 @@ export const Benefits = memo(function Benefits() {
   return (
     <div className="grid grid-cols-container mt-12 md:mt-72 px-4 md:px-8">
       <div className="grid gap-y-14 md:gap-y-20 col-start-2">
-        <div className="text-center md:w-min justify-self-center">
-          <span className="font-bold uppercase text-14 text-96a0db">DISCORD BOUNCER BENEFITS</span>
+        <div className="text-center justify-self-center">
+          <span className="font-bold uppercase text-14 text-ffffff tracking-[0.2em]">WORLD ID BENEFITS</span>
 
-          <GradientText as="h2" className="text-36 md:text-64 font-semibold mt-4 md:whitespace-nowrap">
+          <GradientText as="h2" className="max-w-[560px] mx-auto text-48 font-bold mt-4 uppercase">
             Why choose Discord Bouncer?
           </GradientText>
 
-          <p className="mt-8 md:px-16 md:text-18 text-bcc5f9">
+          <p className="max-w-[688px] mt-8 font-rubik text-18 text-bcc5f9 leading-6">
             Discord Bouncer removes bot spam and protects against sybil attacks on your Discord server. Additionally,
             it’s trustless and completely privacy-preserving.
           </p>
         </div>
-        <div className="grid md:grid-cols-4 gap-4">
+        <div className="grid lg:grid-cols-2 gap-8 pb-28 lg:w-max lg:justify-self-center">
           {items.map((item, index) => (
             <div
               className={cn(
-                'grid place-items-start place-content-start border border-f9f9f9/20 bg-111318 p-8 rounded-lg',
+                'relative lg:even:top-28 lg:max-w-[532px] pt-12 px-12 pb-16 border border-f9f9f9/20 bg-111318 rounded-3xl',
               )}
               key={index}
             >
-              <span className="font-medium text-42 md:text-72">{(index + 1).toString().padStart(2, '0')}</span>
-              <span className="text-20 md:text-24 font-semibold mt-4 md:mt-8">{item.title}</span>
-              <p className="font-rubik text-ffffff/70 mt-4">{item.content}</p>
+              <GradientText as="div" className="text-18 font-semibold">
+                {(index + 1).toString().padStart(2, '0')}
+              </GradientText>
+              <GradientText as="h3" className="mt-12 mb-3 text-32 font-semibold tracking-tight">
+                {item.title}
+              </GradientText>
+              {item.content.map((line, i) => (
+                <p key={i} className="font-rubik text-16 text-bcc5f9 leading-6">
+                  {line}
+                </p>
+              ))}
             </div>
           ))}
         </div>
