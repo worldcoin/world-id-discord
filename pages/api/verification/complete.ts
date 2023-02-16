1219019b0c97f2639f556eb5a98e67d06451d3eb
michaelfurutin <104859710+michaelfurutin@users.noreply.github.com>
2023-02-16 11:02:20 +0300

    feat: implement orb uniqueness (#71)
    Co-authored-by: Igor Osipov <igor.osipov@ottofeller.com>

--- pages/api/verification/complete.ts
+++ pages/api/verification/complete.ts
@@ -1,4 +1,3 @@
-/* eslint-disable complexity -- FIXME */
 import { EmbedBuilder } from '@discordjs/builders'
 import { VerificationCompletePayload } from 'common/types/verification-complete'
 import { APIRole } from 'discord-api-types/v10'
@@ -36,39 +35,37 @@ export default async function handler(req: NextApiRequestWithBody, res: NextApiR
   }
 
   let roleIds: string[]
-  if (result.signal_type === 'orb') {
-    if (!botConfig.orb.enabled) {
-      return await sendErrorResponse(res, token, 400, 'Orb verification is disabled for this server.')
-    }
-    roleIds = botConfig.orb.roles
-  } else if (result.signal_type === 'phone') {
-    const nullifierHashResult = await getNullifierHash({ guild_id: guildId, nullifier_hash: result.nullifier_hash })
 
-    if (!nullifierHashResult.data) {
-      const NullifierSaveResult = await saveNullifier({ guild_id: guildId, nullifier_hash: result.nullifier_hash })
-
-      if (NullifierSaveResult.error) {
-        return await sendErrorResponse(res, token, 500, NullifierSaveResult.error.message)
-      }
-    }
-
-    if (nullifierHashResult.data) {
-      return await sendErrorResponse(
-        res,
-        token,
-        400,
-        'This phone number has already been verified.',
-        'already_verified',
-      )
-    }
-
-    if (!botConfig.phone.enabled) {
-      return await sendErrorResponse(res, token, 400, 'Phone verification is disabled for this server.')
-    }
-    roleIds = botConfig.phone.roles
-  } else {
+  if (result.signal_type !== 'phone' && result.signal_type !== 'orb') {
     return await sendErrorResponse(res, token, 400, 'We had a problem verifying this credential. Please try again.')
   }
+
+  const nullifierHashResult = await getNullifierHash({
+    guild_id: guildId,
+    nullifier_hash: result.nullifier_hash,
+    signal_type: result.signal_type,
+  })
+
+  if (nullifierHashResult.data) {
+    return await sendErrorResponse(res, token, 400, 'This user has already been verified.', 'already_verified')
+  }
+
+  const NullifierSaveResult = await saveNullifier({
+    guild_id: guildId,
+    nullifier_hash: result.nullifier_hash,
+    signal_type: result.signal_type,
+  })
+
+  if (NullifierSaveResult.error) {
+    console.error(NullifierSaveResult.error)
+    return await sendErrorResponse(res, token, 500)
+  }
+
+  if (!botConfig.phone.enabled) {
+    return await sendErrorResponse(res, token, 400, 'Phone verification is disabled for this server.')
+  }
+
+  roleIds = botConfig[result.signal_type].roles
 
   // FIXME: check that these roles really exist on the server
 
@@ -76,10 +73,11 @@ export default async function handler(req: NextApiRequestWithBody, res: NextApiR
     for (const roleId of roleIds) {
       await assignGuildMemberRole(guildId, userId, roleId)
     }
+
     const assignedRoles = guild.roles.filter((role) => roleIds.includes(role.id))
     return await sendSuccessResponse(res, token, assignedRoles)
   } catch (error) {
-    console.error(error)
+    console.error('Error while assigning roles: ', error)
     return await sendErrorResponse(res, token, 500, error?.message)
   }
 }
