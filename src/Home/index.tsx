740aeb564005018980a23bb250384dd4bc0043e4
Michael Furutin <michael.furutin@toolsforhumanity.com>
2023-12-06 16:49:43 +0300

    feat: remove home page

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,39 +1,12 @@
-import { Footer } from 'common/Footer'
-import { Header } from 'common/Header'
-import { InfoLine } from 'common/InfoLine'
-import { Layout } from 'common/Layout'
 import { signIn } from 'next-auth/react'
-import { memo, useCallback } from 'react'
-import { About } from './About'
-import { Benefits } from './Benefits'
-import { Intro } from './Intro'
-import { Join } from './Join'
-import { Methods } from './Methods'
-import { NoCaptcha } from './NoCaptcha'
+import { memo, useEffect } from 'react'
 
 export const Home = memo(function Home() {
-  const handleInstall = useCallback(async () => {
-    await signIn('discord', { callbackUrl: '/admin' })
+
+  // Redirects user to Discord OAuth page
+  useEffect(() => {
+    signIn('discord', { callbackUrl: '/admin' })
   }, [])
 
-  return (
-    <Layout>
-      <div className="grid grid-rows-auto/fr/auto min-h-screen">
-        <Header />
-        <Intro onInstall={handleInstall} />
-
-        <InfoLine
-          className="mt-16 md:mt-0"
-          text="Welcome to the Discord Bouncer, if you want to use this in your server to verify humanness, follow these instructions to get started."
-        />
-      </div>
-      <About />
-      <Methods />
-      <NoCaptcha onInstall={handleInstall} />
-      <Benefits />
-      <Join onInstall={handleInstall} />
-      <InfoLine className="mt-32 md:mt-64" text="This project is open sourced and anyone can contribute to it." />
-      <Footer />
-    </Layout>
-  )
+  return null
 })
