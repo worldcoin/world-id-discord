7c59802f9083c9a0e60e3fc454bd1dfcc426d855
Igor Osipov <igor.osipov@toolsforhumanity.com>
2023-12-12 14:46:39 +0100

    fix: bridge url

--- src/Verification/ErrorScene/index.tsx
+++ src/Verification/ErrorScene/index.tsx
@@ -51,7 +51,6 @@ export const ErrorScene = memo(function ErrorScene(props: {
 
       {props.error !== VerificationError.AlreadyVerified && props.app_id && props.action && props.signal && (
         <IDKitWidget
-          bridge_url="https://staging-bridge.worldcoin.org" // FIXME: use production bridge
           verification_level={credential_types_to_verification_level(props.credentials)}
           app_id={props.app_id}
           action={props.action}
