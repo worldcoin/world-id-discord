e3c5f3fc7c403939d0944377e65e4a91410fb50c
Igor Osipov <igor.osipov@toolsforhumanity.com>
2023-12-12 22:52:35 +0100

    chore: update favicon

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -6,7 +6,7 @@ export const Footer = memo(function Footer() {
     <div className="grid grid-cols-container-wide px-4 md:px-8 py-4 md:py-8">
       <div className="grid items-center grid-flow-col gap-x-4 auto-cols-max col-start-2 text-726aee">
         <Icon className="w-6 h-6" name="logo" />
-        <span className="font-sora font-bold text-14">&copy; 2023 Discord x Worldcoin</span>
+        <span className="font-sora font-bold text-14">&copy; 2023 Tools for Humanity</span>
       </div>
     </div>
   )
