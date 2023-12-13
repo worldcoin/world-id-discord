0ae9a79b2cf0ba7003452badb177f990e4c89b2a
0xPenryn <penryn@pm.me>
2023-12-13 10:25:49 +0100

    fix: action description

--- src/Verification/InitialScene/index.tsx
+++ src/Verification/InitialScene/index.tsx
@@ -56,7 +56,7 @@ export const InitialScene = memo(function Initial(props: {
           app_id={props.app_id}
           action={props.action}
           signal={props.signal}
-          action_description={`verify your uniqueness for ${props.guild.name}`}
+          action_description={'verify your uniqueness for' + props.guild.name}
           onSuccess={props.complete}
         >
           {({ open }: { open: () => void }) => (
