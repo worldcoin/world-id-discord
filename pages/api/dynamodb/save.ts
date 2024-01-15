5d793ab9b9eb52823f6da766996fede46a8d3018
Igor Osipov <igor.osipov@toolsforhumanity.com>
2024-01-15 18:32:53 +0100

    feat: analytics

--- pages/api/dynamodb/save.ts
+++ pages/api/dynamodb/save.ts
@@ -1,9 +1,11 @@
+import { BotConfig } from 'common/types'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { saveBotConfig, verifyBotConfig } from 'services/dynamodb'
+import { PostHogClient } from 'services/posthog'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const botConfig = req.body
-
+  const body = req.body as { botConfig: BotConfig; isFirstConnection: boolean; userId: string }
+  const botConfig = body.botConfig
   const verifiedConfig = verifyBotConfig(botConfig)
 
   if (!verifiedConfig.status) {
@@ -15,6 +17,43 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (!result.success) {
     return res.status(500).json({ message: result.error?.message })
   }
+
+  const posthog = PostHogClient()
+  const userId = body.userId
+
+  if (body.isFirstConnection) {
+    const captureResult = await posthog.capture({
+      event: 'Discord integration guild connected',
+      distinctId: userId,
+
+      properties: {
+        guild_id: botConfig.guild_id,
+        $current_url: `${process.env.NEXTAUTH_URL}/api/dynamodb/save`,
+      },
+    })
+
+    if (!captureResult.success) {
+      console.error(captureResult.error)
+    }
+  }
+
+  const captureResult = await posthog.capture({
+    event: 'Discord integration guild config updated',
+    distinctId: userId,
+
+    properties: {
+      guild_id: botConfig.guild_id,
+      device: JSON.stringify(botConfig.device),
+      orb: JSON.stringify(botConfig.orb),
+      $current_url: `${process.env.NEXTAUTH_URL}/api/dynamodb/save`,
+    },
+  })
+
+  if (!captureResult.success) {
+    console.error(captureResult.error)
+  }
+
+  await posthog.shutdown()
 
   return res.status(200).json({ message: 'OK' })
 }
