5b16943cad3c92398a5c623c5bc20c9c6ce9f31a
Igor Osipov <89008845+igorosip0v@users.noreply.github.com>
2024-01-16 09:54:06 +0100

    Update pages/api/dynamodb/save.ts
    Co-authored-by: pdtfh <149602456+pdtfh@users.noreply.github.com>

--- pages/api/dynamodb/save.ts
+++ pages/api/dynamodb/save.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if (body.isFirstConnection) {
     const captureResult = await posthog.capture({
-      event: 'Discord integration guild connected',
+      event: 'discord integration app added',
       distinctId: userId,
 
       properties: {
