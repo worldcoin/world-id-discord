dadd078e8b1a365b6b833093362a0f81c42f903a
Igor Osipov <89008845+igorosip0v@users.noreply.github.com>
2024-04-12 21:26:34 +0200

    fix: anauthorized save (#296)

--- pages/api/dynamodb/save.ts
+++ pages/api/dynamodb/save.ts
@@ -1,12 +1,26 @@
 import { BotConfig } from 'common/types'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { getToken } from 'next-auth/jwt'
 import { saveBotConfig, verifyBotConfig } from 'services/dynamodb'
 import { PostHogClient } from 'services/posthog'
+import { baseAuthOptions } from '~/pages/api/auth/[...nextauth]'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getServerSession(req, res, baseAuthOptions)
+
+  if (!session) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+
+  const token = await getToken({ req })
   const body = req.body as { botConfig: BotConfig; isFirstConnection: boolean; userId: string }
   const botConfig = body.botConfig
   const verifiedConfig = verifyBotConfig(botConfig)
+
+  if (token?.guildId !== botConfig.guild_id) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
 
   if (!verifiedConfig.status) {
     return res.status(400).json({ message: verifiedConfig.error?.message })
