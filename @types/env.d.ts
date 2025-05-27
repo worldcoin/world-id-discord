e5080837078d30d2f94ce7c9defb4ad01ddb97e4
Igor Osipov <89008845+igorosip0v@users.noreply.github.com>
2025-05-27 15:02:59 +0200

    fix verification error (#301)

---
+++ @types/env.d.ts
@@ -0,0 +1,27 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    DISCORD_APP_ID: string
+    DISCORD_APP_PUBLIC_KEY: string
+    DISCORD_BOT_TOKEN: string
+    DISCORD_APP_SECRET: string
+
+    // App URL
+    NEXTAUTH_URL: string
+    NEXTAUTH_SECRET: string
+
+    // AWS
+    AWS_GUILDS_TABLE_NAME: string
+    AWS_NULLIFIERS_TABLE_NAME: string
+    AWS_DISCORD_BOUNCER_REGION: string
+    AWS_DISCORD_BOUNCER_ACCESS_KEY_ID: string
+    AWS_DISCORD_BOUNCER_SECRET_ACCESS_KEY: string
+
+    // App ID
+    APP_ID: string
+    DEVELOPER_PORTAL_URL: string
+
+    // PostHog
+    POSTHOG_API_KEY: string
+    POSTHOG_HOST: string
+  }
+}
